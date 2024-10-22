2d18f7608e88217a6c9eacd380203564fb67559e
dogukanbalcii <dogukanbalci32@hotmail.com>
2024-10-22 06:43:07 +0300

    Admin Panel | Dashboard
    2024-10-21

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,18 +1,19 @@
 <?php
 
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class DefaultController extends AbstractController
+class AdminController extends AbstractController
 {
-    #[Route('/', name: 'app_default')]
+    #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
         ]);
     }
 }
--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -1,18 +1,19 @@
 <?php
 
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class DefaultController extends AbstractController
+class DashboardController extends AbstractController
 {
-    #[Route('/', name: 'app_default')]
+    #[Route('/dashboard', name: 'dashboard')]
     public function index(): Response
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
         ]);
     }
 }
