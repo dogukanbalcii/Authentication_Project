c8c56ae92f5752b0014c256b9ee7bb5736ea025b
dogukanbalcii <dogukanbalci32@hotmail.com>
2024-10-26 17:09:37 +0300

    Updated TokenSubscriber to refresh the token instead of logging out when it expires.
    2024-10-26

--- src/EventSubscriber/TokenSubscriber.php
+++ src/EventSubscriber/TokenSubscriber.php
@@ -2,19 +2,32 @@
 
 namespace App\EventSubscriber;
 
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\RouterInterface;
+use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 class TokenSubscriber implements EventSubscriberInterface
 {
     private RouterInterface $router;
+    private RefreshTokenManagerInterface $refreshTokenManager;
+    private JWTTokenManagerInterface $jwtTokenManager;
+    private UserProviderInterface $userProvider;
+    private EntityManagerInterface $entityManager;
 
-    public function __construct(RouterInterface $router)
-    {
+    public function __construct(RouterInterface $router, RefreshTokenManagerInterface $refreshTokenManager, JWTTokenManagerInterface $jwtTokenManager, UserProviderInterface $userProvider, EntityManagerInterface $entityManager
+    ) {
         $this->router = $router;
+        $this->refreshTokenManager = $refreshTokenManager;
+        $this->jwtTokenManager = $jwtTokenManager;
+        $this->userProvider = $userProvider;
+        $this->entityManager = $entityManager;
     }
 
     public static function getSubscribedEvents(): array
@@ -34,10 +47,29 @@ class TokenSubscriber implements EventSubscriberInterface
         if (in_array($request->getPathInfo(), $protectedRoutes)) {
             if ($session->has('jwt')) {
                 $token = $session->get('jwt');
-
                 $tokenData = $this->decodeToken($token);
 
                 if ($this->isTokenExpired($tokenData['exp'])) {
+                    if ($session->has('refresh_token')) {
+                        $refreshToken = $session->get('refresh_token');
+                        $refreshTokenObject = $this->refreshTokenManager->get($refreshToken);
+
+                        if ($refreshTokenObject && !$this->isTokenExpired($refreshTokenObject->getValid())) {
+                            $user = $this->userProvider->loadUserByIdentifier($tokenData['username']);
+
+                            $newJwt = $this->jwtTokenManager->create($user);
+
+                            $session->set('jwt', $newJwt);
+                            $user->setToken($newJwt);
+
+                            $this->entityManager->persist($user);
+                            $this->entityManager->flush();
+
+                            return;
+                        }
+                    }
+
+                    // if refresh token is null or expired
                     $event->setResponse(new RedirectResponse($this->router->generate('logout')));
                 }
             } else {
@@ -48,11 +80,15 @@ class TokenSubscriber implements EventSubscriberInterface
 
     private function isTokenExpired($expiration): bool
     {
+        if ($expiration instanceof DateTime) {
+            $expiration = $expiration->getTimestamp();
+        }
+
         return $expiration < time();
     }
 
     private function decodeToken($token)
     {
-        return json_decode(base64_decode(str_replace('_', '/', str_replace('-','+', explode('.', $token)[1]))), true);
+        return json_decode(base64_decode(str_replace('_', '/', str_replace('-', '+', explode('.', $token)[1]))), true);
     }
 }
